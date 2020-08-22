ed8129ef24bb2cd8cda81839ed69d87d0c210665
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 19:23:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,9 +15,7 @@ end
 def get_japanese_emoticon(path,emote)
   emoticons = load_library(path)
   emoticons.each do |key, value|
-    if value[:japanese] == emote
-      puts key
-    end
+    puts value[:japanese]
     
   end
   # code goes here
