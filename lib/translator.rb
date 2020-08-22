bda2b162180b929d13e1c8a9fcc904cccfba507b
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 19:06:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,7 @@ def load_library(path)
   # code goes here
   require "yaml"
   temp_emoticons = YAML.load_file(path)
+  emoticons = {}
   temp_emoticons.each do |index|
     puts emoticons[index[0]]
   end
